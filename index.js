1810bd40e502a2794f6113f7d292bb883bdd74d3
mrshappy0 <ajs11300@gmail.com>
2020-06-03 14:06:40 -0600

    Added a bunch of new styling for computer size screen and more animations

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 let monogram = document.getElementsByClassName("background-img"),
-    slideMenu = document.getElementsByClassName("sidebar");
-    adamShappyLetters = document.getElementsByClassName("absoluteletters")[0].children
-    console.log(adamShappyLetters)
+  slideMenu = document.getElementsByClassName("sidebar");
+adamShappyLetters = document.getElementsByClassName("absoluteletters")[0]
+  .children;
+adamShappyLettersContainer = document.querySelectorAll("section.absoluteletters");
+console.log(adamShappyLettersContainer);
 monogram[0].addEventListener("animationstart", animationListener, false);
 monogram[0].addEventListener("animationend", animationListener, false);
 monogram[0].classList.add("slidein");
+adamShappyLettersContainer[0].addEventListener("mouseover", removeInitialAnime, false);
+
+function removeInitialAnime(event) {
+  if (event.type === "mouseover") {
+    console.log(adamShappyLettersContainer[0].classList[1]);
+    adamShappyLettersContainer[0].classList.remove("remove-after-hover")
+  }
+}
 
 function animationListener(event) {
   if (event.type === "animationstart") {
     Object.keys(slideMenu).forEach((el) => {
       slideMenu[el].classList.add("slideMenu");
     });
-  if (event.type === "animationend"){
-    Object.keys(adamShappyLetters).forEach((el) => {
-      adamShappyLetters[el].classList.add("vibrate");
-      console.log(adamShappyLetters[el].classList)
-    });
-  }
+    if (event.type === "animationend") {
+      Object.keys(adamShappyLetters).forEach((el) => {
+        adamShappyLetters[el].classList.add("vibrate");
+        console.log(adamShappyLetters[el].classList);
+      });
+    }
   }
 }
