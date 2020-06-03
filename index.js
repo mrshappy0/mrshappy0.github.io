b5e2e8d9de440529429b430a6c0472714c0f6307
mrshappy0 <ajs11300@gmail.com>
2020-06-03 14:48:19 -0600

    Added new event listeners for highlighting adam shappy when on info tabs

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 let monogram = document.getElementsByClassName("background-img"),
   slideMenu = document.getElementsByClassName("sidebar");
+  slideMenuContainer = document.querySelector("section.info-tabs")
 adamShappyLetters = document.getElementsByClassName("absoluteletters")[0]
   .children;
-adamShappyLettersContainer = document.querySelectorAll("section.absoluteletters");
-console.log(adamShappyLettersContainer);
+adamShappyLettersContainer = document.querySelectorAll(
+  "section.absoluteletters"
+);
+console.log(slideMenuContainer);
 monogram[0].addEventListener("animationstart", animationListener, false);
 monogram[0].addEventListener("animationend", animationListener, false);
 monogram[0].classList.add("slidein");
-adamShappyLettersContainer[0].addEventListener("mouseover", removeInitialAnime, false);
+adamShappyLettersContainer[0].addEventListener(
+  "mouseover",
+  removeInitialAnime,
+  false
+);
+slideMenuContainer.addEventListener("mouseover", addHoverToAdamLetters, false);
+slideMenuContainer.addEventListener("mouseout", addHoverToAdamLetters, false);
 
 function removeInitialAnime(event) {
   if (event.type === "mouseover") {
-    console.log(adamShappyLettersContainer[0].classList[1]);
-    adamShappyLettersContainer[0].classList.remove("remove-after-hover")
+    adamShappyLettersContainer[0].classList.remove("remove-after-hover");
+  }
+}
+
+function addHoverToAdamLetters(event) {
+  console.log("hit");
+  if (event.type === "mouseover") {
+    adamShappyLettersContainer[0].classList.remove("remove-after-hover");
+    adamShappyLettersContainer[0].classList.add("addEffectIfHover");
+  }
+  if(event.type ==="mouseout"){
+    adamShappyLettersContainer[0].classList.remove("addEffectIfHover");
   }
 }
 
