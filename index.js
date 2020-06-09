5fbb52ae92e64042c8d1ad8dbc52f8faeacad827
mrshappy0 <ajs11300@gmail.com>
2020-06-09 16:52:55 -0600

    testing fix for closing modal with iphone

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ modalBtn.onclick = function () {
 closeBtn.onclick = function () {
   modal.style.display = "none";
 };
-window.onclick = function (e) {
-  if (e.target == modal) {
+modal.onclick = function (e) {
+  // if (e.target == modal) {
     modal.style.display = "none";
-  }
+  // }
 };
