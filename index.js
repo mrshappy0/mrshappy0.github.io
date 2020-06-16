98bcbad29a04eaf54c79d90ca887031ec9275f72
mrshappy0 <ajs11300@gmail.com>
2020-06-16 11:26:17 -0600

    changed out the Adam Shappy gradient and the JavaScript that helped add animation to this

--- index.js
+++ index.js
@@ -1,55 +1,21 @@
 let monogram = document.getElementsByClassName("background-img"),
-  slideMenu = document.getElementsByClassName("sidebar");
-slideMenuContainer = document.querySelector("section.info-tabs");
-adamShappyLetters = document.getElementsByClassName("absoluteletters")[0]
-  .children;
-adamShappyLettersContainer = document.querySelectorAll(
-  "section.absoluteletters"
-);
-scrollContainer = document.querySelector("div.container");
-scrollTopMax =
-  scrollContainer.scrollTopMax ||
-  document.getElementById("about").getBoundingClientRect().height * 3;
-// scrollTopMax = document.getElementById("three").getBoundingClientRect();
+  slideMenu = document.getElementsByClassName("sidebar"),
+  slideMenuContainer = document.querySelector("section.info-tabs"),
+  scrollContainer = document.querySelector("div.container"),
+  scrollTopMax =
+    scrollContainer.scrollTopMax ||
+    document.getElementById("about").getBoundingClientRect().height * 3;
+
+// Initial animation event handling
 monogram[0].addEventListener("animationstart", animationListener, false);
 monogram[0].addEventListener("animationend", animationListener, false);
 monogram[0].classList.add("slidein");
-adamShappyLettersContainer[0].addEventListener(
-  "mouseover",
-  removeInitialAnime,
-  false
-);
-slideMenuContainer.addEventListener("mouseover", addHoverToAdamLetters, false);
-slideMenuContainer.addEventListener("mouseout", addHoverToAdamLetters, false);
-
-function removeInitialAnime(event) {
-  if (event.type === "mouseover") {
-    adamShappyLettersContainer[0].classList.remove("remove-after-hover");
-  }
-}
-
-function addHoverToAdamLetters(event) {
-  if (event.type === "mouseover") {
-    adamShappyLettersContainer[0].classList.remove("remove-after-hover");
-    adamShappyLettersContainer[0].classList.add("addEffectIfHover");
-  }
-  if (event.type === "mouseout") {
-    adamShappyLettersContainer[0].classList.remove("addEffectIfHover");
-  }
-}
 
 function animationListener(event) {
   if (event.type === "animationstart") {
     Object.keys(slideMenu).forEach((el) => {
       slideMenu[el].classList.add("slideMenu");
     });
-
-    // not currently using code below//
-    if (event.type === "animationend") {
-      Object.keys(adamShappyLetters).forEach((el) => {
-        adamShappyLetters[el].classList.add("vibrate");
-      });
-    }
   }
 }
 function resizeWindow() {
@@ -58,64 +24,34 @@ function resizeWindow() {
     document.getElementById("about").getBoundingClientRect().height * 3);
 }
 function scrollHandler() {
-    if (
-      scrollContainer.scrollTop >= scrollTopMax / 4 &&
-      scrollContainer.scrollTop <= (scrollTopMax / 4) * 2
-    ) {
-      slideMenuContainer.childNodes[1].style.color = "red";
-      slideMenuContainer.childNodes[3].style.color = "";
-      slideMenuContainer.childNodes[5].style.color = "";
-    } else if (
-      scrollContainer.scrollTop >= (scrollTopMax / 4) * 2 &&
-      scrollContainer.scrollTop <= (scrollTopMax / 4) * 3
-    ) {
-      slideMenuContainer.childNodes[1].style.color = "";
-      slideMenuContainer.childNodes[3].style.color = "red";
-      slideMenuContainer.childNodes[5].style.color = "";
-    } else if (
-      scrollContainer.scrollTop >= (scrollTopMax / 4) * 3 &&
-      scrollContainer.scrollTop <= (scrollTopMax / 4) * 4
-    ) {
-      slideMenuContainer.childNodes[1].style.color = "";
-      slideMenuContainer.childNodes[3].style.color = "";
-      slideMenuContainer.childNodes[5].style.color = "red";
-    } else {
-      slideMenuContainer.childNodes[1].style.color = "";
-      slideMenuContainer.childNodes[3].style.color = "";
-      slideMenuContainer.childNodes[5].style.color = "";
-    }
+  if (
+    scrollContainer.scrollTop >= scrollTopMax / 4 &&
+    scrollContainer.scrollTop <= (scrollTopMax / 4) * 2
+  ) {
+    slideMenuContainer.childNodes[1].style.color = "red";
+    slideMenuContainer.childNodes[3].style.color = "";
+    slideMenuContainer.childNodes[5].style.color = "";
+  } else if (
+    scrollContainer.scrollTop >= (scrollTopMax / 4) * 2 &&
+    scrollContainer.scrollTop <= (scrollTopMax / 4) * 3
+  ) {
+    slideMenuContainer.childNodes[1].style.color = "";
+    slideMenuContainer.childNodes[3].style.color = "red";
+    slideMenuContainer.childNodes[5].style.color = "";
+  } else if (
+    scrollContainer.scrollTop >= (scrollTopMax / 4) * 3 &&
+    scrollContainer.scrollTop <= (scrollTopMax / 4) * 4
+  ) {
+    slideMenuContainer.childNodes[1].style.color = "";
+    slideMenuContainer.childNodes[3].style.color = "";
+    slideMenuContainer.childNodes[5].style.color = "red";
+  } else {
+    slideMenuContainer.childNodes[1].style.color = "";
+    slideMenuContainer.childNodes[3].style.color = "";
+    slideMenuContainer.childNodes[5].style.color = "";
+  }
 }
 
-// function scrollHandler() {
-//   setTimeout(() => {
-//     if (
-//       scrollContainer.scrollTop >= scrollTopMax / 4 &&
-//       scrollContainer.scrollTop <= (scrollTopMax / 4) * 2
-//     ) {
-//       slideMenuContainer.childNodes[1].style.color = "red";
-//       slideMenuContainer.childNodes[3].style.color = "";
-//       slideMenuContainer.childNodes[5].style.color = "";
-//     } else if (
-//       scrollContainer.scrollTop >= (scrollTopMax / 4) * 2 &&
-//       scrollContainer.scrollTop <= (scrollTopMax / 4) * 3
-//     ) {
-//       slideMenuContainer.childNodes[1].style.color = "";
-//       slideMenuContainer.childNodes[3].style.color = "red";
-//       slideMenuContainer.childNodes[5].style.color = "";
-//     } else if (
-//       scrollContainer.scrollTop >= (scrollTopMax / 4) * 3 &&
-//       scrollContainer.scrollTop <= (scrollTopMax / 4) * 4
-//     ) {
-//       slideMenuContainer.childNodes[1].style.color = "";
-//       slideMenuContainer.childNodes[3].style.color = "";
-//       slideMenuContainer.childNodes[5].style.color = "red";
-//     } else {
-//       slideMenuContainer.childNodes[1].style.color = "";
-//       slideMenuContainer.childNodes[3].style.color = "";
-//       slideMenuContainer.childNodes[5].style.color = "";
-//     }
-//   }, 250);
-// }
 window.addEventListener("resize", () => resizeWindow());
 scrollContainer.addEventListener("scroll", () => scrollHandler());
 
@@ -127,5 +63,5 @@ modalBtn.onclick = function () {
   modal.style.display = "block";
 };
 modal.onclick = function (e) {
-    modal.style.display = "none";
+  modal.style.display = "none";
 };
