7e5500b08105e616e80d81a007d702702fd623d8
mrshappy0 <ajs11300@gmail.com>
2024-03-01 16:40:04 -0700

    generate title helper and helper index

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Generates a title by appending a suffix to the provided title.
+ * @param title - The base title.
+ * @returns The generated title.
+ */
+export const generateTitle = (title: string) => `${title} | Adam Shappy`;
