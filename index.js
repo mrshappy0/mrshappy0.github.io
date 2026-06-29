941b424b8585c2f5ca6caa22366159f37a9f6ef3
mrshappy0 <ajs11300@gmail.com>
2026-06-28 22:03:29 -0600

    Clean up stale vanilla HTML files, add .nojekyll, set package manager for Dependabot

--- index.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-  let sec = document.getElementById("sec");
-  sec.classList.toggle("active");
-
-  let nav = document.getElementById("navigation");
-  nav.classList.toggle("active");
-}
